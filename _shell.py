47f5387225ca66cf4693d1e49134ae7523c06e4d
chrissteingass@gmail.com <chrissteingass@gmail.com>
2018-02-18 18:09:15 -0500

    Generalized path.

--- _shell.py
+++ _shell.py
@@ -19,5 +19,5 @@ import pandas as pd, numpy as np, matplotlib.pyplot as plt
 pd.options.display.max_rows = 1000
 pd.options.display.max_columns = 1000
 
-# Path 
-path = getcwd() + '/Dropbox/_Git/Kaggle_Telstra'
+# Path
+path = getcwd() + '/Dropbox/_Git/'
