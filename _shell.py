f33127f93ccd4e77ca10b9651e2606d668995af3
chrissteingass@gmail.com <chrissteingass@gmail.com>
2018-02-18 23:19:33 -0500

    dynamically create variables based on list of filenames.

--- _shell.py
+++ _shell.py
@@ -7,7 +7,7 @@
 
 #%%
 # -------------------------------------------------------------------------
-# IMPORT LIBRARIES, SET OPTIONS, READ DATA
+# IMPORT LIBRARIES, SET OPTIONS
 # -------------------------------------------------------------------------
 # System
 import os
@@ -16,8 +16,28 @@ from os.path import isfile, join, abspath, exists, isdir
 
 # Base
 import pandas as pd, numpy as np, matplotlib.pyplot as plt
+
+# Options
 pd.options.display.max_rows = 1000
 pd.options.display.max_columns = 1000
+from IPython.core.interactiveshell import InteractiveShell
+InteractiveShell.ast_node_interactivity = "all"
 
 # Path
 path = getcwd() + '/Dropbox/_Git/'
+
+
+# -------------------------------------------------------------------------
+# IMPORT LIBRARIES, SET OPTIONS, READ DATA
+# -------------------------------------------------------------------------
+filenames = [
+
+    ]
+
+for filename in filenames:
+    dataset = pd.read_csv(path + filename)
+    globals()[filename.split('.')[0]] = dataset
+
+    print(filename)
+    dataset.head(3)
+    print('')
